f3f268cb585ed46a7e162c8bf99af406542bdee8
finest08 <mr.mitcholl@gmail.com>
2021-02-23 01:18:06 +1300

    Removed RowCard.css file

--- src/RowCard.js
+++ src/RowCard.js
@@ -2,18 +2,15 @@ import React, { useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import './RowCard.css';
 
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 500,
-    maxHeight: 700,
+    maxHeight: 1200
     
   },
   media: {
@@ -64,21 +61,35 @@ export default function RowCard( { title, fetchUrl} ) {
                                 <Typography gutterBottom variant="p" component="p" align="left">
                                 </Typography>
                                 <Typography component="p" variant="body1" align="left">
-                                    {article.content}
+                                    {article.description}
                                 </Typography>
                             </Typography>
                         </CardContent>
                     </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Share
-                    </Button>
-                    <Button size="small" color="primary">
-                        Learn More
-                    </Button>
-                </CardActions>
             </Card>
             )}    
         </>
     )
 }
+
+
+
+  
+//   max-width: 50px,
+  
+
+// }
+
+// .row {
+//     display: flex;
+    
+// }
+
+// .media {
+//     object-fit: contain;
+    
+//     margin-right: 10px;
+//     height: 0px;
+//     padding-top: 56.25%;
+// }
+
